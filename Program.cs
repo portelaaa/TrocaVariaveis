392d4bc165f53ccf5c8854f512fe57c2309efed9
portelaaa <e107ti@etec.sp.gov.br>
2023-09-05 19:45:03 -0300

    Exercicio Resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Troca de Variaveis");
+Console.WriteLine("");
+
+string nome1, nome2;
+ 
+Console.Write("Digite um nome: ");
+nome1 = Console.ReadLine()!;
+Console.Write("Digite outro nome: ");
+nome2 = Console.ReadLine()!;
+Console.WriteLine("");
+
+Console.WriteLine("Após a troca de valores temos: ");
+Console.WriteLine("");
+
+Console.WriteLine($"Nome 1: {nome2} ");
+Console.WriteLine($"Nome 2: {nome1} ");
